a9724bcd849140842e0cd79ef20c43db0840b013
Szymon Wygnanski <s@finalclass.net>
2014-09-18 15:43:36 +0200

    adding log

--- src/SQLiteTable.js
+++ src/SQLiteTable.js
@@ -1,9 +1,35 @@
 /// <reference path="../../login-server/src/typings/tsd.d.ts"/>
+var debug = require('debug');
+
+var log = debug('sqlite-table');
+var logError = debug('sqlite-table:error');
+
 var SQLiteTable = (function () {
     function SQLiteTable(db) {
         this.db = db;
     }
+    SQLiteTable.prototype.all = function (params, next) {
+        if (typeof params === 'function') {
+            next = params;
+            params = {};
+        }
+        log('get all', params);
+        var stmt = this.getSQLSelectStmt(params);
+        this.db.all(stmt.sql, stmt.objVars, next);
+    };
+
+    SQLiteTable.prototype.find = function (params, next) {
+        var typeofParams = typeof params;
+        if (typeofParams === 'string' || typeofParams === 'number') {
+            params = { id: params };
+        }
+        log('find', params);
+        var stmt = this.getSQLSelectStmt(params);
+        this.db.get(stmt.sql, stmt.objVars, next);
+    };
+
     SQLiteTable.prototype.insert = function (data, next) {
+        log('insert', data);
         var keys = Object.keys(data);
         var keysVars = keys.map(function (key) {
             return '$' + key;
@@ -15,18 +41,10 @@ var SQLiteTable = (function () {
         this.db.run(sql, objVars, next);
     };
 
-    SQLiteTable.prototype.getObjVars = function (obj) {
-        var objVars = {};
-
-        Object.keys(obj).forEach(function (key) {
-            objVars['$' + key] = obj[key];
-        });
-
-        return objVars;
-    };
-
     SQLiteTable.prototype.update = function (data, next) {
+        log('update', data);
         if (!data.id) {
+            logError('id is not specified, can not update');
             next(new Error('id is not specified'));
             return;
         }
@@ -41,6 +59,16 @@ var SQLiteTable = (function () {
         this.db.run(sql, objVars, next);
     };
 
+    SQLiteTable.prototype.getObjVars = function (obj) {
+        var objVars = {};
+
+        Object.keys(obj).forEach(function (key) {
+            objVars['$' + key] = obj[key];
+        });
+
+        return objVars;
+    };
+
     SQLiteTable.prototype.getSQLSelectStmt = function (params) {
         var objVars = this.getObjVars(params);
         var whereStmts = Object.keys(params).map(function (key) {
@@ -52,24 +80,6 @@ var SQLiteTable = (function () {
             sql: 'SELECT * FROM ' + this.tableName + where,
             objVars: objVars
         };
-    };
-
-    SQLiteTable.prototype.all = function (params, next) {
-        if (typeof params === 'function') {
-            next = params;
-            params = {};
-        }
-        var stmt = this.getSQLSelectStmt(params);
-        this.db.all(stmt.sql, stmt.objVars, next);
-    };
-
-    SQLiteTable.prototype.find = function (params, next) {
-        var typeofParams = typeof params;
-        if (typeofParams === 'string' || typeofParams === 'number') {
-            params = { id: params };
-        }
-        var stmt = this.getSQLSelectStmt(params);
-        this.db.get(stmt.sql, stmt.objVars, next);
     };
 
     Object.defineProperty(SQLiteTable.prototype, "tableName", {
--- src/SQLiteTable.ts
+++ src/SQLiteTable.ts
@@ -1,6 +1,10 @@
 /// <reference path="../../login-server/src/typings/tsd.d.ts"/>
 
 import sqlite3 = require('sqlite3');
+import debug = require('debug');
+
+var log = debug('sqlite-table');
+var logError = debug('sqlite-table:error');
 
 class SQLiteTable {
 
@@ -10,7 +14,30 @@ class SQLiteTable {
 
   }
 
+  public all(next:(err?:Error)=>void):void;
+  public all(params?:any, next?:(err?:Error)=>void):void {
+    if (typeof params === 'function') {
+      next = params;
+      params = {};
+    }
+    log('get all', params);
+    var stmt:{sql:string;objVars:any} = this.getSQLSelectStmt(params);
+    this.db.all(stmt.sql, stmt.objVars, next);
+  }
+
+  public find(params:string, next:(err?:Error)=>void):void;
+  public find(params:any, next:(err?:Error)=>void):void {
+    var typeofParams:string = typeof params;
+    if (typeofParams === 'string' || typeofParams === 'number') {
+      params = {id: params};
+    }
+    log('find', params);
+    var stmt:{sql:string;objVars:any} = this.getSQLSelectStmt(params);
+    this.db.get(stmt.sql, stmt.objVars, next);
+  }
+
   public insert(data:any, next:(err?:Error)=>void):void {
+    log('insert', data);
     var keys:string[] = Object.keys(data);
     var keysVars:string[] = keys.map((key:string):string => '$' + key);
     var objVars:any = this.getObjVars(data);
@@ -22,18 +49,10 @@ class SQLiteTable {
     this.db.run(sql, objVars, next);
   }
 
-  private getObjVars(obj):any {
-    var objVars:any = {};
-
-    Object.keys(obj).forEach((key:string):void => {
-      objVars['$' + key] = obj[key];
-    });
-
-    return objVars;
-  }
-
   public update(data:any, next:(err?:Error)=>void):void {
+    log('update', data);
     if (!data.id) {
+      logError('id is not specified, can not update');
       next(new Error('id is not specified'));
       return;
     }
@@ -50,6 +69,16 @@ class SQLiteTable {
     this.db.run(sql, objVars, next);
   }
 
+  private getObjVars(obj):any {
+    var objVars:any = {};
+
+    Object.keys(obj).forEach((key:string):void => {
+      objVars['$' + key] = obj[key];
+    });
+
+    return objVars;
+  }
+
   private getSQLSelectStmt(params:any):{sql:string; objVars:any} {
     var objVars:any = this.getObjVars(params);
     var whereStmts:string[] = Object.keys(params)
@@ -60,25 +89,6 @@ class SQLiteTable {
       sql: 'SELECT * FROM ' + this.tableName + where,
       objVars: objVars
     };
-  }
-  public all(next:(err?:Error)=>void):void;
-  public all(params?:any, next?:(err?:Error)=>void):void {
-    if (typeof params === 'function') {
-      next = params;
-      params = {};
-    }
-    var stmt:{sql:string;objVars:any} = this.getSQLSelectStmt(params);
-    this.db.all(stmt.sql, stmt.objVars, next);
-  }
-
-  public find(params:string, next:(err?:Error)=>void):void;
-  public find(params:any, next:(err?:Error)=>void):void {
-    var typeofParams:string = typeof params;
-    if (typeofParams === 'string' || typeofParams === 'number') {
-      params = {id: params};
-    }
-    var stmt:{sql:string;objVars:any} = this.getSQLSelectStmt(params);
-    this.db.get(stmt.sql, stmt.objVars, next);
   }
 
   public get tableName():string {
